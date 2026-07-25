42fc9edbc74d305a32ea7ea2227b0bfd1e75f2f8
jang-ha-na <j27109252@gmail.com>
2026-07-25 21:46:25 +0900

    Create singup.js

---
+++ singup.js
@@ -0,0 +1,19 @@
+import { auth } from "./firebase.js";
+import {
+  createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+const btn = document.getElementById("signupBtn");
+
+btn.addEventListener("click", async () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await createUserWithEmailAndPassword(auth, email, password);
+    alert("회원가입이 완료되었습니다!");
+    location.href = "login.html";
+  } catch (e) {
+    alert("회원가입 실패: " + e.message);
+  }
+});
