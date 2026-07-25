1258b938b6d850454e1ef0093fe69933cd98e417
jang-ha-na <j27109252@gmail.com>
2026-07-26 01:36:21 +0900

    Update write.js

--- write.js
+++ write.js
@@ -1,18 +1,36 @@
-import { db } from "./firebase.js";
-import { collection, addDoc } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+import { auth, db } from "./firebase.js";
 
-const saveBtn = document.getElementById("saveBtn");
+import {
+  collection,
+  addDoc,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-saveBtn.addEventListener("click", async () => {
+const btn = document.getElementById("writeBtn");
+
+btn.addEventListener("click", async () => {
+  const user = auth.currentUser;
+
+  if (!user) {
+    alert("로그인이 필요합니다.");
+    return;
+  }
+
   const title = document.getElementById("title").value;
   const content = document.getElementById("content").value;
 
-  await addDoc(collection(db, "posts"), {
-    title,
-    content,
-    createdAt: new Date()
-  });
+  try {
+    await addDoc(collection(db, "posts"), {
+      title,
+      content,
+      writer: user.email,
+      uid: user.uid,
+      createdAt: serverTimestamp()
+    });
 
-  alert("저장되었습니다!");
-  location.href = "board.html";
+    alert("글이 등록되었습니다.");
+    location.href = "board.html";
+  } catch (e) {
+    alert(e.message);
+  }
 });
