7abb19c013fb2c66fc922f17aead8f7a8521a84c
jang-ha-na <j27109252@gmail.com>
2026-07-26 01:27:21 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,10 +3,10 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBRLXq0S5k6-v9LcIQm8hKiLGPY-szYve4",
+  apiKey: "AIzaSyBRLXq0S5k6-v9LclQm8hKiLGPY-szYve4",
   authDomain: "hanaprofile.firebaseapp.com",
   projectId: "hanaprofile",
-  storageBucket: "hanaprofile.firebasestorage.app",
+  storageBucket: "hanaprofile.appspot.com",
   messagingSenderId: "511043861506",
   appId: "1:511043861506:web:037501a7c9c26fc19fbb1b",
   measurementId: "G-QP6R4KBPN0"
