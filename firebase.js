ac5c62d15d87d9ef1ad41b4a94434eedcd0c601a
jang-ha-na <j27109252@gmail.com>
2026-07-25 23:13:31 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBRLXq0S5k6-v9LclQm8hKiLGPY-szYve4",
+  apiKey: "AIzaSyBRLXq0S5k6-v9LcIQm8hKiLGPY-szYve4",
   authDomain: "hanaprofile.firebaseapp.com",
   projectId: "hanaprofile",
   storageBucket: "hanaprofile.firebasestorage.app",
@@ -16,12 +12,7 @@ const firebaseConfig = {
   measurementId: "G-QP6R4KBPN0"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 
 const auth = getAuth(app);
 const db = getFirestore(app);
