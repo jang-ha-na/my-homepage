591ac75f2b180f8ce880eb556cc158f38364a210
jang-ha-na <j27109252@gmail.com>
2026-07-25 22:38:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 import { auth } from "./firebase.js";
 import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
-const loginLink = document.getElementById("loginLink");
+window.addEventListener("DOMContentLoaded", () => {
+  const loginLink = document.getElementById("loginLink");
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      loginLink.textContent = "로그아웃";
+      loginLink.href = "#";
+
+      loginLink.onclick = async (e) => {
+        e.preventDefault();
+        await signOut(auth);
+        alert("로그아웃되었습니다.");
+        location.reload();
+      };
+    }
+  });
+});
 
 onAuthStateChanged(auth, (user) => {
   if (user) {
