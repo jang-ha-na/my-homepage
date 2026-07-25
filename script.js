c3a58164f76d2d3625312979508d384b0f9d9dec
jang-ha-na <j27109252@gmail.com>
2026-07-26 08:51:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,11 @@
 const menuBtn = document.getElementById("menuBtn");
 const menu = document.getElementById("menu");
-const darkBtn = document.getElementById("darkBtn");
-const topBtn = document.getElementById("topBtn");
 
-// 햄버거 메뉴
-if(menuBtn){
-    menuBtn.addEventListener("click", () => {
-        menu.classList.toggle("show");
-    });
-}
-
-// 다크모드
-if(darkBtn){
-    darkBtn.addEventListener("click", () => {
-        document.body.classList.toggle("dark");
-
-        darkBtn.textContent =
-            document.body.classList.contains("dark")
-            ? "☀️"
-            : "🌙";
-    });
-}
-
-// 맨 위 버튼
-window.addEventListener("scroll", () => {
-    if(topBtn){
-        topBtn.style.display =
-            window.scrollY > 300 ? "block" : "none";
-    }
+menuBtn.addEventListener("click", () => {
+    menu.classList.toggle("show");
 });
 
-if(topBtn){
-    topBtn.addEventListener("click", () => {
-        window.scrollTo({
-            top:0,
-            behavior:"smooth"
-        });
-    });
-}
-document.querySelectorAll("#menu a").forEach(link => {
+document.querySelectorAll("#menu a[href^='#']").forEach(link => {
     link.addEventListener("click", () => {
         menu.classList.remove("show");
     });
