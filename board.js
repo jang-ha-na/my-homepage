b356c9a35b0dd8aaf8b641a2dd3012e1e192d495
jang-ha-na <j27109252@gmail.com>
2026-07-26 02:17:54 +0900

    Update board.js

--- board.js
+++ board.js
@@ -1,21 +1,24 @@
 import { db } from "./firebase.js";
-import { collection, getDocs } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+import {
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const postList = document.getElementById("postList");
 
 async function loadPosts() {
+
   const querySnapshot = await getDocs(collection(db, "posts"));
 
+  postList.innerHTML = "";
+
   querySnapshot.forEach((doc) => {
+
     const post = doc.data();
 
     postList.innerHTML += `
       <div class="card">
-        <h3>${post.title}</h3>
-        <p>${post.content}</p>
-      </div>
-    `;
-  });
-}
-
-loadPosts();
+        <h3>
+          <a href="view.html?id=${doc.id}">
+            ${
