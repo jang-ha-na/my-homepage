35863c8506cda9383bb3b8b1ffb97b4e4e4d108e
jang-ha-na <j27109252@gmail.com>
2026-07-26 12:41:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -13,11 +13,18 @@ document.querySelectorAll("#menu a[href^='#']").forEach(link => {
 
 const darkBtn = document.getElementById("darkBtn");
 
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+}
+
 if (darkBtn) {
     darkBtn.addEventListener("click", () => {
         document.body.classList.toggle("dark");
+
+        if (document.body.classList.contains("dark")) {
+            localStorage.setItem("theme", "dark");
+        } else {
+            localStorage.setItem("theme", "light");
+        }
     });
-}
-if (localStorage.getItem("theme") === "dark") {
-    document.body.classList.add("dark");
 }
