fdbbce3620902dfa9a23ff2bb7592d903aee103a
jang-ha-na <j27109252@gmail.com>
2026-07-26 12:34:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ document.querySelectorAll("#menu a[href^='#']").forEach(link => {
         menu.classList.remove("show");
     });
 });
+
+const darkBtn = document.getElementById("darkBtn");
+
+if (darkBtn) {
+    darkBtn.addEventListener("click", () => {
+        document.body.classList.toggle("dark");
+
+        if (document.body.classList.contains("dark")) {
+            localStorage.setItem("theme", "dark");
+        } else {
+            localStorage.setItem("theme", "light");
+        }
+    });
+}
+
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+}
